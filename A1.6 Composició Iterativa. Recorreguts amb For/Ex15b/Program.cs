c4cbe20535e6dabd8856e25dcacd51524348f3cd
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-15 09:40:42 +0100

    artur

---
+++ A1.6 Composició Iterativa. Recorreguts amb For/Ex15b/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex15b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string linia;
+            int n;
+
+            Console.Write("ENTRA UN NUMERO: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                linia = GenerarLinia('$', n);
+                Console.WriteLine(linia);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string GenerarLinia(char car, int longitud)
+        {
+            string str;
+            str = "";
+
+            for(int i = 1; i <= longitud; i++)
+            {
+                str = str + car;
+            }
+
+            return str;
+        }
+
+    }
+}
