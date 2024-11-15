b61e0e9dc512cc38b6b49c8c4f235d3df2d0d0c6
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-15 08:31:14 +0100

    artur

---
+++ A1.6 Composició Iterativa. Recorreguts amb For/Ex14/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string FILE_NAME = "tiquetsuper.txt";
+
+            int totalProductes;
+            double preuTiquet;
+            string linia;
+            string nom;
+            double quantitat;
+            double preu;
+            StreamReader fTiquet;
+
+            //inicialitzar
+            fTiquet = new StreamReader(FILE_NAME);
+            preuTiquet = 0;
+
+            //llegir el total de productes
+
+            //linia = fTiquet.ReadLine();
+            //totalProductes = Convert.ToInt32(linia);
+            totalProductes = Convert.ToInt32(fTiquet.ReadLine());
+
+            for(int i = 1; i <= totalProductes; i++)
+            {
+                //llegir poducte
+                nom = fTiquet.ReadLine();
+                quantitat = Convert.ToDouble(fTiquet.ReadLine());
+                preu = Convert.ToDouble(fTiquet.ReadLine());
+
+                //tractar producte
+                preuTiquet = preuTiquet + quantitat * preu;
+            }
+
+            Console.WriteLine($"EL PREU DEL TIQUET ÉS {preuTiquet}");
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
