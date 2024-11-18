96fccfef70b6388b5b11e0564829c29b29ecc143
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-18 11:33:57 +0100

    A1.6 Ex17

---
+++ A1.6 Composició Iterativa. Recorreguts amb For/Ex17/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MostrarTaula(1);
+            //MostrarTaula(2);
+            //MostrarTaula(3);
+            //MostrarTaula(4);
+
+            for(int i = 0; i <= 10; i++)
+            {
+                MostrarTaula(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void MostrarTaula(int numTaula)
+        {
+            Console.WriteLine($"TAULA MULTIPLICAR {numTaula}");
+            Console.WriteLine("---");
+
+            for(int i=1;i<=10;i++)
+                Console.WriteLine($"{numTaula}x{i}={numTaula*i}");
+        }
+
+
+
+    }
+}
