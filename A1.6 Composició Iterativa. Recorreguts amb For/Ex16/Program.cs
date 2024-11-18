0608c0f2039071b3a6263e44afef9e5270c9fc28
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-18 11:22:34 +0100

    A1.6 Ex16

---
+++ A1.6 Composició Iterativa. Recorreguts amb For/Ex16/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("ENTRA UN NUMERO: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                //mostrem de 1 a i asterisc
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ A1.6 Composició Iterativa. Recorreguts amb For/Ex16b/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string linia;
+            int n;
+
+            Console.Write("ENTRA UN NUMERO: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                //generar una línia de longitud 'i'
+                linia = GenerarLinia('*', i);
+                Console.WriteLine(linia);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static string GenerarLinia(char car, int longitud)
+        {
+            string str;
+            str = "";
+
+            for (int i = 1; i <= longitud; i++)
+            {
+                str = str + car;
+            }
+
+            return str;
+        }
+    }
+}
