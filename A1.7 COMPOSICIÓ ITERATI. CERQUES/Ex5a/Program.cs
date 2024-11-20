d3d6856a8fd09268a2c5f568eb0bc7a4ba3999d6
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-20 12:15:11 +0100

    A1.7 Ex5a

---
+++ A1.7 COMPOSICIÓ ITERATI. CERQUES/Ex5a/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int suma;
+
+            Console.Write("ENTRA UN NUMERO: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            suma = 0;
+
+            for (int i=1; i<=num/2;i++)
+            {
+                if (num % i == 0)
+                {
+                    suma = suma + i;
+                }
+
+            }
+
+            if(suma==num)
+                Console.WriteLine("ES PERFECTE");
+            else
+                Console.WriteLine("NO ES PERFECTE");
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
