4e36dff9e360c4c17103f31e42df70ca6d4aeaef
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-11-27 12:53:20 +0100

    artur

---
+++ A1.8 COMPOSICIÓ ITERATIVA. FINESTRES/Ex1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FILE_NAME = "NUMEROS2.TXT";
+
+            //variables
+            StreamReader sr;
+            string linia;
+            bool trobat;
+
+            int numActual=0;
+            int num2=0;
+            int num3=0;
+
+            //inicialitzacions
+            sr = new StreamReader(FILE_NAME);
+            trobat = false;
+
+            //llegir elements de la finestra
+            linia = sr.ReadLine();
+            if(linia != null)
+            {
+                numActual = Convert.ToInt32(linia);
+                linia = sr.ReadLine();
+                if (linia != null)
+                {
+                    num2 = numActual;
+                    numActual = Convert.ToInt32(linia);
+                    linia = sr.ReadLine();
+                }
+            }
+
+            while (!trobat && linia != null)
+            {
+                //tractar l'element
+                num3 = num2;
+                num2 = numActual;
+                numActual = Convert.ToInt32(linia);
+
+                if (numActual == num3 + num2)
+                {
+                    trobat = true;
+                }
+                else
+                {
+                    linia = sr.ReadLine();
+                }
+            }
+
+            if (trobat)
+            {
+                Console.WriteLine($"S'HA TROBAT LA CONDICIÓ: {num3}+{num2}={numActual}");
+            } else
+            {
+                Console.WriteLine("NO S'HA TROBAT CAP CONDICIÓ");
+            }
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
