0a4a98b2a74c956b0271efb10920d1cf72e56ded
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-12-04 11:24:31 +0100

    artur

---
+++ NF 2 Introducció a la Programació OO/MenuPremier/MenuPremier/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuPremier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msg"></param>
+        public static void MsgNextScreen(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Es demana per teclat l'abreviatura d'un equip i s'informa si l'equip existeix o no. 
+        /// En cas que existeixi, es mostren les dades de l'equip
+        /// </summary>
+        /// <param name="fileTeams">fitxer que conté tots els equips</param>
+        public static void DoSearchTeam(string fileTeams)
+        {
+                     
+        }
+        /// <summary>
+        /// Es retorna el nom de l'equip a partir de la seva abreviatura
+        /// </summary>
+        /// <param name="fileTeams">fitxer que conté els equips</param>
+        /// <param name="abreviatura">abreviatura de l'equip a cercar</param>
+        /// <returns>el nom de l'equip trobat en el fitxer fileTeams que tingui com a abreviatura el valor del paràmetre 'abreviatura'
+        /// si l'equip no existeix, retornem null</returns>
+        public static string GetTeam(string fileTeams, string abreviatura)
+        {
+            return "";
+        }
+        /// <summary>
+        /// demana una abreviatura d'equip per teclat. 
+        /// Si l'equip existeix, mostra el nom i els gols totals fet per l'equip en tots els seus partits.
+        /// Si no existeix, es dona un msg d'error i tornem al menú principal
+        /// </summary>
+        /// <param name="fileTeams"></param>
+        /// <param name="fileMatches"></param>
+        public static void DoGetGoalsTeam(string fileTeams, string fileMatches)
+        {
+            Console.WriteLine("EXECUCIÓ DEL CODI DE l'ACCIÓ DoGetGoalsTeam");
+            MsgNextScreen("PREM UNA TECLA PER TORNAR AL MENÚ PRINCIPAL");
+        }
+ 
+        public static void MostrarMenu()
+        {
+
+        }
+    }
+}
