84ca6818bb25a7465cbd74068fd008ecc82414ec
ajuvi <102821845+ajuvi@users.noreply.github.com>
2024-12-20 10:34:29 +0100

    artur

---
+++ NF 2 Introducció a la Programació OO/INTRO OOP/PROJECTE EMPRESA/PROJECTE EMPRESA/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTE_EMPRESA
+{
+    internal class Employee
+    {
+        #region Atributs
+        private int id;
+        private string firstName;
+        private string lastName;
+        private double salary;
+        private double commission;
+        private DateTime hireDate;
+
+        public Employee(int id, string firstName, 
+                        string lastName, double salary, 
+                        double commission, DateTime hireDate)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.salary = salary;
+            this.commission = commission;
+            this.hireDate = hireDate;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Getter and setters
+
+        #endregion
+
+        #region Mètodes
+
+
+        public override string ToString()
+        {
+            return $"{this.id},{this.firstName},{this.lastName},{this.salary}";
+        }
+        #endregion
+
+
+    }
+}
---
+++ NF 2 Introducció a la Programació OO/INTRO OOP/PROJECTE EMPRESA/PROJECTE EMPRESA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECTE_EMPRESA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1;
+            Employee e2;
+            e1 = new Employee(1, "Pere", "Vidal", 12000, 0.2, DateTime.Now);
+            e2 = new Employee(2, "Maria", "Esteve", 15000, 0.3, DateTime.Now);
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
+        }
+    }
+}
