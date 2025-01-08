7d9f3643f3567e10f70a058bd83f40e99f40af32
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-01-08 10:44:55 +0100

    artur

--- NF 2 Introducció a la Programació OO/INTRODUCCIO OOP/PROJECTE EMPRESA/PROJECTE EMPRESA/Employee.cs
+++ NF 2 Introducció a la Programació OO/INTRODUCCIO OOP/PROJECTE EMPRESA/PROJECTE EMPRESA/Employee.cs
@@ -8,7 +8,9 @@ namespace PROJECTE_EMPRESA
 {
     internal class Employee
     {
-        #region Atributs
+
+        #region "Atributs"
+
         private int id;
         private string firstName;
         private string lastName;
@@ -16,9 +18,33 @@ namespace PROJECTE_EMPRESA
         private double commission;
         private DateTime hireDate;
 
-        public Employee(int id, string firstName, 
-                        string lastName, double salary, 
-                        double commission, DateTime hireDate)
+        #endregion
+
+        #region Getters i Setters
+
+        /// <summary>
+        /// Obtenir el cognom de l'empleat
+        /// </summary>
+        /// <returns>El cognom de l'empleat</returns>
+        public string GetLastName()
+        {
+            return this.lastName;
+        }
+
+        /// <summary>
+        /// Assignar un valor a la comissió
+        /// </summary>
+        /// <param name="value">Nou valor de comissió</param>
+        public void SetCommission(double value)
+        {
+            this.commission = value;
+        }
+
+        #endregion
+
+        # region Constructors
+
+        public Employee(int id, string firstName, string lastName, double salary, double commission, DateTime hireDate)
         {
             this.id = id;
             this.firstName = firstName;
@@ -28,33 +54,40 @@ namespace PROJECTE_EMPRESA
             this.hireDate = hireDate;
         }
 
-        #endregion
-
-        #region Constructors
-
-        #endregion
-
-        #region Getter and setters
-
-        public string GetLastName()
-        {
-            return this.lastName;
-        } 
-
-        public void SetCommission(double value)
-        {
-            this.commission = value;
-        }
+        public Employee(int id, string firstName, string lastName, double salary) : this(id, firstName, lastName, salary, 0, DateTime.Now)
+        { }
 
         #endregion
 
         #region Mètodes
 
+        /// <summary>
+        /// Calcula el salari total d'un empleat a partir del salari i de la comissió.
+        /// </summary>
+        /// <returns>El salari total</returns>
+        public double SalariTotal()
+        {
+            return salary * (1 + commission);
+        }
 
+        /// <summary>
+        /// Calcula el nombre d'anys que ha treballat un empleat.
+        /// </summary>
+        /// <returns>Nombre d'anys treballats</returns>
+        public double AnysTreballats()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Converteix un empleat en format text.
+        /// </summary>
+        /// <returns>L'empleat en format text</returns>
         public override string ToString()
         {
-            return $"{this.id},{this.firstName},{this.lastName},{this.salary}";
+            return $"{id};{firstName};{lastName};{salary};{commission};{hireDate}";
         }
+
         #endregion
 
 
