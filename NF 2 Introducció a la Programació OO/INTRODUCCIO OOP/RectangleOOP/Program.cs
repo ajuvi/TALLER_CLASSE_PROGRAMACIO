811dd9a022bcb3aca4728a582cb3cdbd221f3dba
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-01-10 08:39:51 +0100

    artur

---
+++ NF 2 Introducció a la Programació OO/INTRODUCCIO OOP/RectangleOOP/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace RectangleOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Crear els rectangles de la figura
+            Rectangle r1 = new Rectangle(2, 2, 1, 4);
+            Rectangle r2 = new Rectangle(2, 2, 2, 3);
+            Rectangle r3 = new Rectangle(3, 2, 2, 0);
+
+            //2) Mostrar l’àrea dels rectangles r1 i r2
+            Console.WriteLine($"AREA R1: {r1.Area}");
+            Console.WriteLine($"AREA R2: {r2.Area}");
+
+            //3) Comprovar si el rectangle r3 és quadrat
+            if (r3.EsQuadrat())
+                Console.WriteLine("EL RECTANGLE 3 ÉS QUADRAT");
+            else
+                Console.WriteLine("EL RECTANGLE 3 NO ÉS QUADRAT");
+
+            //4) Calcular l’àrea màxima dels tres rectangles
+            double areaMaxima;
+            areaMaxima = Math.Max(r1.Area, r2.Area);
+            areaMaxima = Math.Max(areaMaxima,r3.Area);
+
+            Console.WriteLine($"AREA MAXIMA: {areaMaxima}");
+
+            //5) Moure el rectangle r1 a la posició(0, 0)
+            r1.Moure(0, 0);
+
+            //6) Crear un nou rectangle(r4) sumant els rectangles r2 i r3
+            Rectangle r4 = Rectangle.Suma(r2, r3);
+
+            //7 Mostrar els tres rectangles per pantalla
+            Console.WriteLine(r1);
+            Console.WriteLine(r2);
+            Console.WriteLine(r3);
+            Console.WriteLine(r4);
+        }
+    }
+}
---
+++ NF 2 Introducció a la Programació OO/INTRODUCCIO OOP/RectangleOOP/Rectangle.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleOOP
+{
+    public class Rectangle
+    {
+        #region Atributs
+        private double amplada;
+        private double alçada;
+        private double x;
+        private double y;
+        #endregion
+
+        #region Constructors
+        public Rectangle(double amplada, double alçada, double x, double y)
+        {
+            if (amplada <= 0 || alçada <= 0)
+                throw new Exception("L'AMPLADA I L'ALÇADA EL RECTANGLE HAN DE SER VALORS POSITIUS.");
+
+            this.amplada = amplada;
+            this.alçada = alçada;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Rectangle() : this(1, 1, 0, 0) { }
+
+        #endregion
+
+        #region Propietats
+        public double X
+        {
+            get { return this.x; }
+            set { this.x = value; }
+        }
+        public double Y
+        {
+            get { return this.y; }
+            set { this.y = value; }
+        }
+
+        public double Amplada
+        {
+            get { return this.amplada; }
+            set
+            {
+                if (value <= 0) throw new Exception("L'AMPLADA DEL RECTANGLE HA DE SER UN VALOR POSITIU.");
+                this.amplada = value;
+            }
+        }
+        public double Alçada
+        {
+            get { return this.alçada; }
+            set
+            {
+                if (value <= 0) throw new Exception("L'ALÇADA DEL RECTANGLE HA DE SER UN VALOR POSITIU.");
+                this.alçada = value;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return this.amplada * this.alçada;
+            }
+        }
+        #endregion
+
+        #region Mètodes d'instancia
+
+        public double Perimetre()
+        {
+            return this.amplada * this.alçada * 2;
+        }
+
+        public bool EsQuadrat()
+        {
+            return this.amplada == this.alçada;
+        }
+
+        public void Moure(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"{{w:{this.Amplada},h:{this.Alçada},x:{this.X},y:{this.Y}}}";
+        }
+
+
+
+        #endregion
+
+        #region Mètodes de classe
+
+        public static double MaxArea(Rectangle r1, Rectangle r2)
+        {
+            return Math.Max(r1.Area, r2.Area);
+        }
+
+        /// <summary>
+        /// Genera un nou rectangle amb la suma de les alçades i les amplades de dos rectangles.
+        /// </summary>
+        /// <param name="r1">Primer rectangle</param>
+        /// <param name="r2">Segon rectangle</param>
+        /// <returns>Un nou rectangle amb la suma de les alçades i les amplades de dos rectangles.</returns>
+        /// <exception cref="Exception">Si algun dels rectangles és null.</exception>
+        public static Rectangle Suma(Rectangle r1, Rectangle r2)
+        {
+            if (r1 == null || r2 == null)
+                throw new Exception("ELS DOS RECTANGLES NO PODEN SER NULL.");
+
+            Rectangle nou = new Rectangle();
+            nou.Amplada = r1.Amplada + r2.Amplada;
+            nou.Alçada = r1.Alçada + r2.Alçada;
+
+            return nou;
+        }
+
+        #endregion
+    }
+}
