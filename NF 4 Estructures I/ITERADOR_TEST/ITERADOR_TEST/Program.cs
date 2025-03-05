d89f75e4cb2ba848bef249a26199f03750cbd3c3
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-03-05 12:00:43 +0100

    ITERADOR_TEST Actualización automática - 05/03/2025

---
+++ NF 4 Estructures I/ITERADOR_TEST/ITERADOR_TEST/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITERADOR_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static IEnumerable<int> NextValue(int inici, int final)
+        {
+            if (inici > final)
+                throw new Exception();
+
+            int num = inici;
+            while (num<=final)
+            {
+                num++;
+                yield return num;
+            }
+
+        }
+
+    }
+}
