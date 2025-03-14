3b3cf65a635ae95291ce01df270b20319c3f410c
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-03-14 11:08:19 +0100

    IENUMERATOR Actualización automática - 14/03/2025

---
+++ NF 4 Estructures I/POKEMONS_ALUMNE/POKEMONS/EnumeradorTaulaLlista.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POKEMONS
+{
+    internal class EnumeradorTaulaLlista<T> : IEnumerator<T>
+    {
+
+        private int nElem;
+        private int posicio;
+        private T[] dades;
+
+        public EnumeradorTaulaLlista(T[] dades, int nElem) { 
+            this.dades = dades; 
+            this.posicio = -1; 
+            this.nElem = nElem; 
+        }
+
+        public T Current {
+            get {
+                return dades[posicio];
+            }
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            posicio++;
+
+            return posicio >= 0 && posicio < nElem;
+        }
+
+        public void Reset()
+        {
+            posicio = -1;
+        }
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ NF 4 Estructures I/POKEMONS_ALUMNE/POKEMONS/Program.cs
@@ -0,0 +1,184 @@
+﻿using POKEMONS.INFRAESRUCTURA;
+using POKEMONS.MODEL;
+using System.ComponentModel.Design;
+
+namespace POKEMONS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaulaLlista<Pokemon> pokemons = new TaulaLlista<Pokemon>();
+            ConsoleKey op;
+
+            do
+            {
+                Console.Clear();
+                Menu();
+                Console.Write("ENTRA UNA OPCIÓ: ");
+                op = Console.ReadKey().Key;
+
+                SaltDeLinia(2);
+
+                switch (op)
+                {
+                    case ConsoleKey.D1:
+                        DoAddPokemon(pokemons);
+                        break;
+                    case ConsoleKey.D2:
+                        DoListPokemonsForEach(pokemons);
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+                    case ConsoleKey.D3:
+                        DoListPokemonsIterador(pokemons);
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+                    case ConsoleKey.D4:
+                        DoDeletePokemon(pokemons);
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+                    case ConsoleKey.D5:
+                        DoDeletePokemonList(pokemons);
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+                    case ConsoleKey.D6:
+                        DoCarregarPokemons(pokemons);
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+                    case ConsoleKey.D0:
+                        NextScreen("\nGOODBYE!");
+                        break;
+                    default:
+                        Console.WriteLine("TECLA NO VÀLIDA");
+                        NextScreen("\nPREM UNA TECLA PER CONTINUAR...");
+                        break;
+
+                }
+
+            } while (op != 0);
+            
+        }
+
+        private static void DoCarregarPokemons(TaulaLlista<Pokemon> pokes)
+        {
+            //eliminem tots els pokemons
+            pokes.Clear();
+            
+            //crear el proveidor de pokemons
+            PokemonProvider provide = new PokemonProvider();
+
+            //afegir els pokemons a la nostra llista
+            IEnumerable<Pokemon> coleccio = provide.GetAll();
+
+            /*foreach(Pokemon p in coleccio)
+            {
+                pokes.Add(p);
+            }*/
+
+            //pokes.AddRange(coleccio);
+
+
+
+
+
+        }
+
+        /// <summary>
+        /// Demana a l'usuari les dades d'un Pokemon i l'afageix a la llista passada per paràmetre.
+        /// </summary>
+        /// <param name="pokes">TaulaLlista de tipus Pokemon</param>
+        private static void DoAddPokemon(TaulaLlista<Pokemon> pokes)
+        {
+            Console.Write("ENTRA UN NOM: ");
+            string nom = Console.ReadLine();
+
+            Console.Write("ENTRA L'ATAC [0-100]: ");
+            int atac = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("ENTRA LA DEFENSA [0-100]: ");
+            int defensa = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("ENTRA LA VELOCITAT [0-100]: ");
+            int velocitat = Convert.ToInt32(Console.ReadLine());
+
+            pokes.Add(new Pokemon(nom,atac,defensa,velocitat));
+        }
+
+        /// <summary>
+        /// Mostra el nombre de pokemons que hi ha a la llista i mostra tots els pokemons de la llista utilitzant el foreach.
+        /// </summary>
+        /// <param name="pokes">TaulaLlista de tipus Pokemon</param>
+        private static void DoListPokemonsForEach(TaulaLlista<Pokemon> pokes)
+        {
+            Console.WriteLine($"TOTAL POKEMONS: {pokes.Count}");
+
+            foreach (Pokemon p in pokes)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        /// <summary>
+        /// Mostra el nombre de pokemons que hi ha a la llista i mostra tots els pokemons de la llista utilitzant un iterador.
+        /// </summary>
+        /// <param name="pokes">TaulaLlista de tipus Pokemon</param>
+        private static void DoListPokemonsIterador(TaulaLlista<Pokemon> pokes)
+        {
+            Console.WriteLine($"TOTAL POKEMONS: {pokes.Count}");
+
+            IEnumerator<Pokemon> iterador = (IEnumerator<Pokemon>)pokes.GetEnumerator();
+
+            while (iterador.MoveNext())
+                Console.WriteLine(iterador.Current);
+        }
+
+        /// <summary>
+        /// Demana el nom d'un Pokemon de la llista i l'elimina.
+        /// Informa a l'usuari su el pokemon ha estat eliminat.
+        /// </summary>
+        /// <param name="pokes">TaulaLlista de tipus Pokemon</param>
+        private static void DoDeletePokemon(TaulaLlista<Pokemon> pokes)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Elimina tots els pokemons de la llista.
+        /// Mostra per pantalla quants elements han estat eliminats.
+        /// </summary>
+        /// <param name="pokes">TaulaLlista de tipus Pokemon</param>
+        /// <exception cref="NotImplementedException"></exception>
+        private static void DoDeletePokemonList(TaulaLlista<Pokemon> pokes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("POKEMONS COLLECTION");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("1. AFEGIR UN POKEMON");
+            Console.WriteLine("2. LLISTAR POKEMONS (FOREACH)");
+            Console.WriteLine("3. LLISTAR POKEMONS (ITERADOR)");
+            Console.WriteLine("4. ELIMINAR UN POKEMON");
+            Console.WriteLine("5. ELIMINAR TOTS ELS POKEMONS");
+            Console.WriteLine("6. CARREGAR POKEMONS");
+            Console.WriteLine("0. SORTIR");
+            Console.WriteLine();
+        }
+
+        private static void NextScreen(String str)
+        {
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+
+        private static void SaltDeLinia(int n)
+        {
+            for(int i=0;i<n;i++) Console.WriteLine();
+        }
+
+
+
+    }
+}
