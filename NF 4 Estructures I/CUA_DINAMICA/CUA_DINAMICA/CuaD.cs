bc5dc73020e95866d35c5500d7c26cb8d5fad510
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-03-31 12:30:30 +0200

    CUA DINAMICA Actualización automática - 31/03/2025

---
+++ NF 4 Estructures I/CUA_DINAMICA/CUA_DINAMICA/CuaD.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUA_DINAMICA
+{
+    public class CuaD<T>:IEnumerable<T>
+    {
+        private Node head;
+        private Node tail;
+        private int nElem;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Node
+        {
+            private T data;
+            private Node next;
+
+            public Node(T dada)
+            {
+                this.data = dada;
+                this.next = null;
+            }
+            public T Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+            public Node Next
+            {
+                get { return next; }
+                set { next = value; }
+            }
+        }
+
+
+    }
+}
---
+++ NF 4 Estructures I/CUA_DINAMICA/CUA_DINAMICA/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("CUA_DINAMICA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CUA_DINAMICA")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("cdeee7e2-593e-4a35-9811-825278e1d8ca")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
