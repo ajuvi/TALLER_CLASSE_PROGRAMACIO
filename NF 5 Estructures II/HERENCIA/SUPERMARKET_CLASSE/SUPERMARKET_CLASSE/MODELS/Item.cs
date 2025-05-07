b9a8c6b320ba82faae51b2eb8bdb0270c9c4a1b6
ajuvi <102821845+ajuvi@users.noreply.github.com>
2025-05-07 11:33:46 +0200

    ENUMERADOR Actualización automática - 07/05/2025

---
+++ NF 5 Estructures II/HERENCIA/SUPERMARKET_CLASSE/SUPERMARKET_CLASSE/MODELS/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUPERMARKET_CLASSE.MODELS
+{
+    internal class Item:IComparable<Item>
+    {
+
+        public enum Category
+        { BEVERAGE = 1, FRUITS, VEGETABLES, BREAD, MILK_AND_DERIVATES, GARDEN, MEAT, SWEETS, SAUCES, FROZEN, CLEANING, FISH, OTHER };
+        public enum Packaging { Unit='U', Kg='K', Package='P' };
+
+        public int CompareTo(Item? other)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- NF 5 Estructures II/HERENCIA/SUPERMARKET_CLASSE/SUPERMARKET_CLASSE/Program.cs
+++ NF 5 Estructures II/HERENCIA/SUPERMARKET_CLASSE/SUPERMARKET_CLASSE/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace SUPERMARKET_CLASSE
+﻿using SUPERMARKET_CLASSE.MODELS;
+
+namespace SUPERMARKET_CLASSE
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Item.Category categoria;
+            categoria = (Item.Category)2;
+            Console.WriteLine(categoria);
+
+            Item.Packaging packaging;
+            packaging = (Item.Packaging)'U';
+            Console.WriteLine(packaging);
+
+
         }
     }
 }
